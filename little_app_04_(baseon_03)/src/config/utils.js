1e90c1ec264548d5ac3af763daf011c4627f0535
LinearLaw <17817363770@163.com>
2018-09-28 11:23:01 +0800

    update view pdf file

--- little_app_04_(baseon_03)/src/config/utils.js
+++ little_app_04_(baseon_03)/src/config/utils.js
@@ -1,5 +1,6 @@
 
 let utils = {
+    //提示信息
     toast (_text,_icon){
         wx.showToast({
             title:_text || "",
@@ -8,6 +9,22 @@ let utils = {
               setTimeout(()=>{
                 wx.hideToast();
               },1500);
+            }
+        })
+    },
+    //查看pdf/doc/excel
+    viewDocument(url){
+        wx.downloadFile({
+            url:  url,
+            success: function (res) {
+              var Path = res.tempFilePath;//返回的文件临时地址
+              wx.openDocument({
+                filePath: Path,
+                success: function (res) {}
+              })
+            },
+            fail: function (res) {
+              console.log("fail",res);
             }
         })
     }
